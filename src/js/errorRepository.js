cf3b3ad47e97a45592d666d595a87136f61b9fca
Vakabunga <shesterov@gmail.com>
2020-05-25 23:29:57 +0300

    create class ErrorRepository

--- src/js/errorRepository.js
+++ src/js/errorRepository.js
@@ -1,3 +1,19 @@
 export default class ErrorRepository {
-  
+  static addError(key, value) {
+    this.dbErrors.set(key, value);
+  }
+
+  static translate(code) {
+    if (this.dbErrors.has(code)) {
+      return this.dbErrors.get(code);
+    }
+    return 'Unknown error';
+  }
 }
+
+ErrorRepository.dbErrors = new Map([
+  [1, 'out of order'],
+  [2, 'connection error'],
+  [3, 'invalid name or password'],
+  [404, 'not found'],
+]);
